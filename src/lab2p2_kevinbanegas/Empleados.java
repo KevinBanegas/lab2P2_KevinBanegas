3dbadae7a9c94ae6e7a04c73abcab73995111f9d
KevinBanegas <kevinandre0504@gmail.com>
2022-07-29 15:04:12 -0600

    Progress en Empleados

--- src/lab2p2_kevinbanegas/Empleados.java
+++ src/lab2p2_kevinbanegas/Empleados.java
@@ -1,14 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab2p2_kevinbanegas;
 
-/**
- *
- * @author Usuario
- */
 public class Empleados {
+
+    private String nombre;
+    private int edad;
+    private double sueldo;
+    private String RRHH;
+    private boolean trabajando;
+    public Empleados(){
+        
+    }
+    public Empleados(String nombre, int edad, double sueldo, String RRHH) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sueldo = sueldo;
+        this.RRHH = RRHH;
+        this.trabajando = false;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setSueldo(Double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setRRHH(String RRHH) {
+        this.RRHH = RRHH;
+    }
+
+    public String getRRHH() {
+        return RRHH;
+    }
+
+    public void setTrabajando(int trabajando) {
+        if (trabajando == 1 || trabajando == 2) {
+            if (trabajando == 1) {
+                this.trabajando = true;
+            } else {
+                this.trabajando = false;
+            }
+        }
+    }
+
+    public boolean getTrabajando() {
+        return trabajando;
+    }
     
+    public String toString(){
+        return "Nombre: "+nombre+"\nEdad: "+edad+"\nSueldo: "+sueldo+"\nRRHH: "+RRHH+"Trabajando: "+trabajando;
+    }
+    
+
 }
--- src/lab2p2_kevinbanegas/Lab2P2_KevinBanegas.java
+++ src/lab2p2_kevinbanegas/Lab2P2_KevinBanegas.java
@@ -1,47 +1,106 @@
 package lab2p2_kevinbanegas;
+
+import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Lab2P2_KevinBanegas {
-    static Scanner lea =  new Scanner(System.in);
+
+    static Scanner lea = new Scanner(System.in);
+
     public static void main(String[] args) {
+        ArrayList<Object> listaEmp = new ArrayList();
+
         boolean centinela = true;
         String ingresar = "admin";
         String ingresar1 = "admin1234";
         System.out.println("--------Iniciar Sesion--------");
         System.out.println("Ingrese usuario:");
         String usuario = lea.next();
-        lea =  new Scanner(System.in);
+        lea = new Scanner(System.in);
         System.out.println("Ingrese contrasena:");
         String contra = lea.next();
-        if(ingresar.equals(usuario) && ingresar1.equals(contra)){
-            while (centinela == true){
+        if (ingresar.equals(usuario) && ingresar1.equals(contra)) {
+            while (centinela == true) {
                 System.out.println("1) Empleados");
                 System.out.println("2) Cientes");
                 System.out.println("3) Carros");
                 System.out.println("4) Salir");
                 int opcion = lea.nextInt();
-                switch(opcion){
-                    case 1:{
-                        
+                lea = new Scanner(System.in);
+                switch (opcion) {
+                    case 1: { //Empleados
+                        boolean centinelaEmp = true;
+                        while (centinelaEmp == true) {
+                            System.out.println("-----EMPLEADOS-----");
+                            System.out.println("1) Crear");
+                            System.out.println("2) Modificar");
+                            System.out.println("3) Eliminar");
+                            System.out.println("4) Listar Empleado");
+                            System.out.println("5) Salir");
+                            System.out.println("-------------------");
+                            System.out.println("Ingrese una opcion:");
+                            int opcionEmp = lea.nextInt();
+                            lea = new Scanner(System.in);
+                            switch (opcionEmp) {
+                                case 1: {
+                                    System.out.println("Ingrese Nombre:");
+                                    String nombre = lea.nextLine();
+                                    lea = new Scanner(System.in);
+                                    System.out.println("Ingrese Edad:");
+                                    int edad = lea.nextInt();
+                                    lea = new Scanner(System.in);
+                                    System.out.println("Ingrese Sueldo:");
+                                    double sueldo = lea.nextDouble();
+                                    lea = new Scanner(System.in);
+                                    System.out.println("Ingrese RRHH:");
+                                    String RRHH =lea.next();
+                                    lea = new Scanner(System.in);
+
+                                    listaEmp.add(new Empleados(nombre,edad,sueldo,RRHH));
+                                }
+                                break;
+                                case 2: {
+                                    for (Object object : listaEmp) {
+                                        System.out.println(listaEmp.indexOf(object) +" - "+object);
+                                        System.out.println();
+                                    }
+                                    
+                                    System.out.println("Ingrese el indice del empleado");
+                                    int emp = lea.nextInt();
+                                }
+                                break;
+                                case 3: {
+
+                                }
+                                break;
+                                case 4: {
+
+                                }
+                                case 5: {
+
+                                }
+                            }
+                        }
                     }
                     break;
-                    case 2:{
-                        
+                    case 2: {
+
                     }
                     break;
-                    case 3:{
-                        
+                    case 3: {
+
                     }
                     break;
-                    case 4:{
-                        
+                    case 4: {
+
                     }
                     break;
-                    default:{
-                        
+                    default: {
+
                     }
                 }
             }
         }
     }
-    
+
 }
