ead5126c48721e50d72ac11b786aaf7d99cd3661
KevinBanegas <kevinandre0504@gmail.com>
2022-07-29 14:09:44 -0600

    Primer commit

--- src/lab2p2_kevinbanegas/Lab2P2_KevinBanegas.java
+++ src/lab2p2_kevinbanegas/Lab2P2_KevinBanegas.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Lab2P2_KevinBanegas {
     static Scanner lea =  new Scanner(System.in);
     public static void main(String[] args) {
+        boolean centinela = true;
         String ingresar = "admin";
         String ingresar1 = "admin1234";
         System.out.println("--------Iniciar Sesion--------");
@@ -12,8 +13,33 @@ public class Lab2P2_KevinBanegas {
         System.out.println("Ingrese contrasena:");
         String contra = lea.next();
         if(ingresar.equals(usuario) && ingresar1.equals(contra)){
-            while (opcion != 4){
-                System.out.println("");
+            while (centinela == true){
+                System.out.println("1) Empleados");
+                System.out.println("2) Cientes");
+                System.out.println("3) Carros");
+                System.out.println("4) Salir");
+                int opcion = lea.nextInt();
+                switch(opcion){
+                    case 1:{
+                        
+                    }
+                    break;
+                    case 2:{
+                        
+                    }
+                    break;
+                    case 3:{
+                        
+                    }
+                    break;
+                    case 4:{
+                        
+                    }
+                    break;
+                    default:{
+                        
+                    }
+                }
             }
         }
     }
